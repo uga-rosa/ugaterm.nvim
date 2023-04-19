e7823077d233404593318f09a72b496660c4324c
uga-rosa <uga6603@gmail.com>
2023-04-20 02:15:37 +0900

    test: check whether to exist commands

---
+++ lua/ugaterm/init_spec.lua
@@ -0,0 +1,15 @@
+local ugaterm = require("ugaterm")
+
+describe("Test for init.lua", function()
+  it("create_commands()", function()
+    ugaterm.create_commands()
+    -- `2` means full match with a command
+    assert.equals(2, vim.fn.exists(":UgatermOpen"))
+    assert.equals(2, vim.fn.exists(":UgatermNew"))
+    assert.equals(2, vim.fn.exists(":UgatermHide"))
+    assert.equals(2, vim.fn.exists(":UgatermToggle"))
+    assert.equals(2, vim.fn.exists(":UgatermDelete"))
+    assert.equals(2, vim.fn.exists(":UgatermSelect"))
+    assert.equals(2, vim.fn.exists(":UgatermRename"))
+  end)
+end)
